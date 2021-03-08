a7aea3a6f2978b94d0fa3c1f930498d3f9f386d8
DevDosSantosLucas <lucas1995ls@outlook.com>
2021-03-08 20:47:21 -0300

    2 - Conceitos importantes

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
 module.exports ={
-    mode: 'development',
+    mode: isDevelopment? 'development': 'production',
+    devtool: isDevelopment? 'eval-source-map': 'source-map',
     entry: path.resolve(__dirname,'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -10,17 +15,34 @@ module.exports ={
     resolve:{
         extensions: ['.js', '.jsx'],
     },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        hot: true,
+    },
     plugins:[
+        isDevelopment && new ReactRefreshWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public','index.html')
         })
-],
+    ].filter(Boolean),
     module: {
         rules: [
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/ ,
-                use: 'babel-loader'
+                use: {
+                    loader:'babel-loader',
+                    options:{
+                        plugins:[
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/ ,
+                use: ['style-loader','css-loader','sass-loader']
             }
         ]
     }
