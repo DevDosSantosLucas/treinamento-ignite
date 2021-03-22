025f4037a1bf76c25a2cddda36af150136563326
DevDosSantosLucas <lucas1995ls@outlook.com>
2021-03-22 20:02:17 -0300

    config mirage

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,12 +1,14 @@
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
 import {GlobalStyle} from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>HELLO WORLD</h1>
+    <>
+      <Header/>
+      <Dashboard />
       <GlobalStyle />
-
-    </div>
+    </>
   );
 }
 
--- 02-dtmoney/src/index.tsx
+++ 02-dtmoney/src/index.tsx
@@ -1,6 +1,24 @@
+import { createServer } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './App';
+createServer({
+  routes(){
+    this.namespace = 'api';
+    this.get('/transactions',()=>{
+      return (
+        {
+          id:1,
+          title: 'transaction 1',
+          amount:400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date(), 
+        }
+      )
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
