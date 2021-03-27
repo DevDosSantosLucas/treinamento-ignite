59da2b16e4d9d0f559ee950f3277acd535fcf55c
DevDosSantosLucas <lucas1995ls@outlook.com>
2021-03-27 00:00:09 -0300

    Modal&Forms completed

--- 02-dtmoney/src/index.tsx
+++ 02-dtmoney/src/index.tsx
@@ -1,4 +1,4 @@
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './App';
@@ -6,19 +6,41 @@ import {App} from './App';
 
 
 createServer({
+  models:{
+    transaction: Model,
+  },
+  seeds(server){
+    server.db.loadData({
+      transactions:[
+        {
+          id:1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-01-12 09:00:00'),
+        },
+        {
+          id:2,
+          title: 'aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-01-14 11:00:00'),
+        }
+      ]
+    })
+  },
+
   routes(){
     this.namespace = 'api';
     this.get('/transactions',()=>{
-      return (
-        {
-          id:1,
-          title: 'transaction 1',
-          amount:400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date(), 
-        }
-      )
+      return  this.schema.all('transaction')
+    })
+
+    this.post('/transactions',(schema,request) =>{
+      const data = JSON.parse(request.requestBody)
+      return schema.create('transaction', data)
     })
   }
 })
