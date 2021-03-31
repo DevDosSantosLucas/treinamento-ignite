42263399b2431b1a8bfc937e779162c388abe66b
DevDosSantosLucas <lucas1995ls@outlook.com>
2021-03-31 18:31:36 -0300

    Componentes e paginas

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,14 +1,28 @@
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 
-import Head from 'next/head'
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
   <>
     <Head>
-      <title>Inicio | ig.news</title>
+      <title>Home | ig.news</title>
     </Head>
-    <h1 >
-      HeLLO WORLD
-    </h1>
+    
+    <main className = {styles.contentContainer}>
+      <section className = {styles.hero}>
+        <span>👏 Hey, welcome</span>
+        <h1>News about the <span>React</span> world.</h1>
+        <p>
+          GetAccess to all the publications<br/>
+          <span>for $ 9.90 month</span>
+        </p>
+        <SubscribeButton />
+      </section>
+      
+      <img src ="/images/avatar.svg" alt="Girl coding"/>
+    </main>
   </>
   )
 }
