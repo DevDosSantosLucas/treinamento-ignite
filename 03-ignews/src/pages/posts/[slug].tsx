0da93322f47fab83380fb7c868118222612699c2
DevDosSantosLucas <lucas1995ls@outlook.com>
2021-08-06 21:32:16 -0300

    static preview

--- 03-ignews/src/pages/posts/[slug].tsx
+++ 03-ignews/src/pages/posts/[slug].tsx
@@ -38,11 +38,12 @@ export default function Post ({post}: PostProps){
     )
 } 
 
+ 
 export const getServerSideProps: GetServerSideProps = async ({req,params})=>{
     const session =await getSession({req})
     const {slug}= params;
     console.log(session)
-    if(!session.activeSubscription){
+    if(!session?.activeSubscription){
         return {
             redirect: {
                 destination: '/',
